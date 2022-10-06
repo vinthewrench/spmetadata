ae582a7f4d7cd65e86d8feb6cb3fac228d26e5aa
Vinnie Moscaritolo <vinnie@vmeng.com>
2022-10-06 09:46:45 -0700

    no message

--- src/MetaDataMgr.cpp
+++ src/MetaDataMgr.cpp
@@ -183,6 +183,9 @@ bool MetaDataMgr::openOutput(const char* path, speed_t speed, int &error){
 	options.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
 	options.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
 	
+	options.c_ispeed=speed;
+	options.c_ospeed=speed;
+
   	if (cfsetospeed (&options, speed) < 0){
 		fprintf (stderr, "FAIL cfsetospeed %d  %s\n", speed, strerror(errno));
 		error = errno;
